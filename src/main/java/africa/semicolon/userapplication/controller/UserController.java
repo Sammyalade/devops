eea5916a0c48c009e0856bfe9c8b14ded654de97
Izuchukwu Ijeudo <151368567+Izunna-24@users.noreply.github.com>
2024-08-27 03:55:23 +0100

    login

--- src/main/java/africa/semicolon/userapplication/controller/UserController.java
+++ src/main/java/africa/semicolon/userapplication/controller/UserController.java
@@ -1,6 +1,8 @@
 package africa.semicolon.userapplication.controller;
 
+import africa.semicolon.userapplication.dtos.request.SignInRequest;
 import africa.semicolon.userapplication.dtos.request.SignUpRequest;
+import africa.semicolon.userapplication.dtos.response.SignInResponse;
 import africa.semicolon.userapplication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,4 +23,15 @@ public class UserController {
     public ResponseEntity<?> signUp(@RequestBody SignUpRequest signUpRequest){
         return new ResponseEntity<>(userService.signUp(signUpRequest), HttpStatus.OK);
     }
+
+    @PostMapping("/signIn")
+    public ResponseEntity<SignInResponse> loginUser(@RequestBody SignInRequest signIn) {
+        SignInResponse response = userService.signIn(signIn);
+        if (response.getMessage().equals("Login successful")) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+        }
+    }
 }
+
--- src/main/java/africa/semicolon/userapplication/services/UserServiceImpl.java
+++ src/main/java/africa/semicolon/userapplication/services/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package africa.semicolon.userapplication.services;
 
+import africa.semicolon.userapplication.dtos.request.SignInRequest;
 import africa.semicolon.userapplication.dtos.request.SignUpRequest;
 import africa.semicolon.userapplication.dtos.response.RegisterResponse;
+import africa.semicolon.userapplication.dtos.response.SignInResponse;
 import africa.semicolon.userapplication.entity.User;
 import africa.semicolon.userapplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -19,4 +23,19 @@ public class UserServiceImpl implements UserService{
         repository.save(user);
         return new RegisterResponse(user.getUsername(),user.getPassword());
     }
+
+    @Override
+    public SignInResponse signIn(SignInRequest signIn) {
+        Optional<User> user = repository
+                .findByUsername(signIn.getUsername());
+
+        if (user.isEmpty() || !user.get()
+                .getPassword().equals(signIn.getPassword())) {
+            return new SignInResponse(null,"invalid credential");
+        }
+        User userIn = user.get();
+        return new SignInResponse(userIn.getUsername(),"Singed in successful");
+    }
 }
+
+
