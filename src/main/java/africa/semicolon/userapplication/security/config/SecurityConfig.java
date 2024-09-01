c9cf6933c1bcd86741866da046b9be8090765766
Aladeyelu Olufemi Samuel <151362938+Sammyalade@users.noreply.github.com>
2024-09-01 12:24:14 +0100

    securityImpl

--- src/main/java/africa/semicolon/userapplication/security/config/SecurityConfig.java
+++ src/main/java/africa/semicolon/userapplication/security/config/SecurityConfig.java
@@ -2,44 +2,58 @@ package africa.semicolon.userapplication.security.config;
 
 import africa.semicolon.userapplication.security.filter.CustomAuthorizationFilter;
 import africa.semicolon.userapplication.security.filter.CustomUsernamePasswordAuthenticationFilter;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.POST;
+import java.util.List;
+
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final AuthenticationManager authenticationManager = new ProviderManager();
 
     private final CustomAuthorizationFilter authorizationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = authenticationManager(http);
+
         var authenticationFilter = new CustomUsernamePasswordAuthenticationFilter(authenticationManager);
         authenticationFilter.setFilterProcessesUrl("/api/v1/auth");
+
         return http.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .sessionManagement(c->c.sessionCreationPolicy(STATELESS))
-                .addFilterAt(authenticationFilter, BasicAuthenticationFilter.class)
+                .sessionManagement(c -> c.sessionCreationPolicy(STATELESS))
+                .addFilterAt(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(authorizationFilter, CustomUsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(c->
+                .authorizeHttpRequests(c ->
                         c.requestMatchers("/api/v1/auth/register").permitAll()
-                            .requestMatchers("/api/v1/auth/login").permitAll()
+                                .requestMatchers("/api/v1/auth/login").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(authenticationProvider())
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return (AuthenticationProvider) new ProviderManager();
     }
 }
